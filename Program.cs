5960107a756f3631e1f86b0f78b4df3d43497e18
KRJ <KRJ@A00-SHARAFI>
2021-03-14 13:25:43 +0330

    thr commit

--- Program.cs
+++ Program.cs
@@ -16,11 +16,14 @@ namespace SrvSampleBuild
             CreateHostBuilder(args).Build().Run();
         }
 
+
+
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    
                 });
     }
 }
